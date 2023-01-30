c452cc1c1db05deec7a4c37218b4f7882b77440f
Sammer.Sun <Sammer.Sun@gaiaworks.cn>
2023-01-30 23:01:06 +0800

    m

--- day02/code/DartLearn/02_Dart的函数使用/01_函数的基本使用.dart
+++ day02/code/DartLearn/02_Dart的函数使用/01_函数的基本使用.dart
@@ -2,9 +2,7 @@
  * @Author: Sammer.Sun Sammer.Sun@gaiaworks.cn
  * @Date: 2020-02-24 20:58:54
  * @LastEditors: Sammer.Sun Sammer.Sun@gaiaworks.cn
- * @LastEditTime: 2023-01-30 20:40:09
- * @FilePath: /Flutter资料day01~23/day02/code/DartLearn/02_Dart的函数使用/01_函数的基本使用.dart
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @LastEditTime: 2023-01-30 22:00:13
  */
 main(List<String> args) {
     print(sum(20.1, 222.20));
